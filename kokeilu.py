03ae630c0684c0f80691aec7096be8ede73a8b4d
Marko Lajunen <marko.lajunen@gmail.com>
2024-11-21 12:20:39 +0200

    tulotmenot

--- kokeilu.py
+++ kokeilu.py
@@ -1,20 +1,27 @@
 import tkinter as tk
 from tkinter import messagebox
+import TulotMenot  # Tuo TulotMenot-moduuli
 
+def avaa_tulotmenot_ikkuna():
+    TulotMenot.main()
 
 def main():
     # Luo pääikkuna
     root = tk.Tk()
-    root.title("Tulot ja Menot")
+    root.title("Kokeilu")
 
     # Lisää käyttöliittymäelementtejä
-    aloi = tk.Label(root, text="Tervetuloa Tulot ja Menot -ohjelmaan!")
+    aloi = tk.Label(root, text="Tervetuloa Kokeilu-ohjelmaan!")
     aloi.pack()
 
-   # Lisää tekstilaatikko
+    # Lisää tekstilaatikko
     tekstilaatikko = tk.Text(root, width=40, height=10)
     tekstilaatikko.pack()
-    
+
+    # Lisää TulotMenot-ikkunan avaamispainike
+    tulotmenot_painike = tk.Button(root, text="Avaa TulotMenot", command=avaa_tulotmenot_ikkuna)
+    tulotmenot_painike.pack()
+
     # Lisää sulkupainike
     sulje_painike = tk.Button(root, text="Sulje", command=root.quit)
     sulje_painike.pack()
@@ -24,6 +31,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
- 
-    
