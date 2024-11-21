f54067109d9d17f9d55d2a37526a683b7c334b21
Marko Lajunen <marko.lajunen@gmail.com>
2024-11-21 12:34:53 +0200

    juu

--- kokeilu.py
+++ kokeilu.py
@@ -1,30 +1,31 @@
 import tkinter as tk
 from tkinter import messagebox
-import TulotMenot  # Tuo TulotMenot-moduuli
+import importlib
 
-def avaa_tulotmenot_ikkuna():
+def tuo_tulotmenot():
+    TulotMenot = importlib.import_module('TulotMenot')
     TulotMenot.main()
 
 def main():
     # Luo pääikkuna
     root = tk.Tk()
-    root.title("Kokeilu")
+    root.title("Tulot ja Menot")
 
     # Lisää käyttöliittymäelementtejä
-    aloi = tk.Label(root, text="Tervetuloa Kokeilu-ohjelmaan!")
+    aloi = tk.Label(root, text="Tervetuloa Tulot ja Menot -ohjelmaan!")
     aloi.pack()
 
     # Lisää tekstilaatikko
     tekstilaatikko = tk.Text(root, width=40, height=10)
     tekstilaatikko.pack()
 
-    # Lisää TulotMenot-ikkunan avaamispainike
-    tulotmenot_painike = tk.Button(root, text="Avaa TulotMenot", command=avaa_tulotmenot_ikkuna)
+    # Lisää TulotMenot-moduulin tuomispainike
+    tulotmenot_painike = tk.Button(root, text="Tuo TulotMenot", command=tuo_tulotmenot)
     tulotmenot_painike.pack()
 
     # Lisää sulkupainike
-    sulje_painike = tk.Button(root, text="Sulje", command=root.quit)
-    sulje_painike.pack()
+    #sulje_painike = tk.Button(root, text="Sulje", command=root.quit)
+    #sulje_painike.pack()
 
     # Käynnistä pääsilmukka
     root.mainloop()
